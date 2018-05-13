9e20c8740c12ab8736cd48f15754fe55817101bb
nzweben <nzweben@votem.com>
2018-05-13 17:15:46 -0400

    added tooltips

--- src/Menu.js
+++ src/Menu.js
@@ -5,19 +5,21 @@ import Play from './images/icons/play.svg';
 import Record from './images/icons/record.svg';
 import Reset from './images/icons/reset.svg';
 import Share from './images/icons/share.svg';
+import ReactTooltip from 'react-tooltip'
 
 function Menu(props) {
 	return (
 		<div className="menu">
-			<img src={Record} className="menuButton record" />
-			<img onClick={props.reset} src={Reset} className="menuButton" />
+			<ReactTooltip place="bottom" type="light" effect="solid"/>
+			<img data-tip="Record" onTouchStart={props.record} onTouchStop={props.stopRecord} src={Record} className="menuButton record" />
+			<img data-tip="Clear All" onClick={props.reset} src={Reset} className="menuButton" />
 			{!props.paused ? (
-				<img onClick={props.pause} src={Pause} className="menuButton " />
+				<img data-tip="Pause" onClick={props.pause} src={Pause} className="menuButton " />
 			) : (
-				<img onClick={props.play} src={Play} className="menuButton" />
+				<img data-tip="Play" onClick={props.play} src={Play} className="menuButton" />
 			)}
-			<img src={Share} className="menuButton " />
-			<img onClick={props.help} src={Help} className="menuButton " />
+			<img data-tip="Share Beat" src={Share} className="menuButton " />
+			<img data-tip="Help" onClick={props.help} src={Help} className="menuButton " />
 		</div>
 	);
 }
